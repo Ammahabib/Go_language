d51f2242bad7006cfde895cfaeb6a243249e46cf
Ammahabib <38022204+Ammahabib@users.noreply.github.com>
2018-06-20 11:47:42 +0500

    Rename struct to struct.go

---
+++ struct.go
@@ -0,0 +1,31 @@
+package main 
+import ("fmt")
+const usixteenbitmax float64 = 65535
+const kmh_multiple float64 =1.60934
+type car struct {
+	gas_pedal uint16 // min 0 max 65535
+	break_pedal uint16
+	steering_wheel int16
+	top_speed_kmh float64 //-32k - +32k
+
+}
+
+func(c car ) kmh() float64{
+//associating a function to a type method 
+//officially a methhod 
+	
+	return float64(c.gas_pedal)*(c.top_speed_kmh/usixteenbitmax) 
+
+} 
+
+
+func main () {
+	 
+	 a_car := car{22341,0,12561,225.0}
+	 fmt.Println(a_car.gas_pedal)
+	 fmt.Println(a_car.kmh())
+	 
+	
+}
+
+
